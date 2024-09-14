f6a4c15e16979379a4e2e5fabc984e7e4368b973
maneeshjohn <maneeshjohno@gmail.com>
2024-09-14 13:17:06 +0530

    added bank and account struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::u32;
+
+#[derive(Debug)]
+struct Account {
+    id: i32,
+    holder: String,
+    balance: u32
+}
+
+impl Account {
+    fn new(holder: String, balance: u32) -> Self {
+        return Account {
+            id: 1,
+            holder,
+            balance
+        };
+    }
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>
+}
+
+impl Bank {
+    fn new() -> Bank {
+        return Bank {
+            accounts: vec![]
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let account = Account::new("test".to_owned(), 1000);
+    let bank = Bank::new();
+
+    println!("Bank: {:?}, Account: {:?}", bank, account);
 }
